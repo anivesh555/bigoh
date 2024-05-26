341a4106acd637dcd19c55ef9cdd8031588f63ed
Anivesh_0777 <v-anivesh.b@mindtickle.com>
2024-05-26 08:49:54 +0530

    refactored

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,34 +1,4 @@
-// const { Pool } = require('pg');
-// const constants = require("../utilities/constants");
 
-
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'indigo',
-//     password: '123',
-//     port: 8100, 
-//   });
-
-// pool.on('error', (err) => {
-//     console.error('Error connecting to the database:', err);
-//   });
-
-// pool.on('connect', () => {  
-//   console.log('Connected to the database');
-// });
-// pool.connect((err, client, release) => {
-//   if (err) {
-//     console.error('Error acquiring client:', err);
-//     return;
-//   }
-
-
-//   // Don't forget to release the client when done with it
-//   release();
-  
-
-// });
 const { Pool } = require('pg');
 const constants = require("../utilities/constants");
 
